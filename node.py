00dfed3f4e17675cfbf48f6acd141bbb01033a69
Aadya21370 <aadya21370@iiitd.ac.in>
2024-03-28 22:34:22 +0530

    simplified cache function

--- node.py
+++ node.py
@@ -54,86 +54,29 @@ def terminate(id):
     sys.exit(0)
 
 
-class CacheNode:
-    key = None
-    value = None
-    pre = None
-    next = None
+class Cache:
+    def __init__(self):
+        self.mapping = {}
+        self.order = []
 
-    def __init__(self,key,value):
-        self.key = key
-        self.value = value
-
-class LRUCache:
-    mapping = {}
-    head = None
-    end = None
-
-    def __init__(self,capacity):
-        self.capacity = capacity
-
-    def set(self,key,value):
+    def set(self, key, value):
         if key in self.mapping:
-            oldNode = self.mapping[key]
-            if value is None:
-                self.mapping.pop(key)
-                self.remove(oldNode)
-                #self.getallkeys()
-                #print('erase key', key)
-            else:
-                oldNode.value = value
-                self.remove(oldNode)
-                self.sethead(oldNode)
-        else:
-            node = CacheNode(key,value)
-            if len(self.mapping) >= self.capacity:
-                self.mapping.pop(self.end.key)
-                self.remove(self.end)
-                self.sethead(node)
-            else:
-                self.sethead(node)
-            self.mapping[key] = node
+            self.order.remove(key)
+        self.mapping[key] = value
+        self.order.append(key)
 
-    def sethead(self,node):
-        node.next = self.head
-        node.pre = None
-        if self.head:
-            self.head.pre = node
-        self.head = node
-        if not self.end:
-            self.end = self.head
+    def printcache(self):
+        if self.mapping:
+            print('current cache is', end=' ')
+            for key, value in self.mapping.items():
+                print(f'<{key}, {value}>', end=', ')
+            print()
 
-    def remove(self,node):
-        if node.pre:
-            node.pre.next = node.next
-        else:
-            self.head = node.next
-        if node.next:
-            node.next.pre = node.pre
-        else:
-            self.end = node.pre
-
-    def getallkeys(self):
-        tmp = None
-        if self.head:
-            tmp = self.head
-            pair = "<{0},{1}>"
-            res = ""
-            while tmp:
-                tmp_p = pair.format(tmp.key,tmp.value)
-                res += tmp_p
-                res += "->"
-                tmp = tmp.next
-            tmp_p = pair.format(None,None)
-            res += tmp_p
-            print('current cache is ', res)
-
-    def get(self,key):
+    def get(self, key):
         if key in self.mapping:
-            node = self.mapping[key]
-            self.remove(node)
-            self.sethead(node)
-            return node.value
+            self.order.remove(key)
+            self.order.append(key)
+            return self.mapping[key]
         else:
             return None
 
@@ -153,8 +96,7 @@ class Node():
         self.commitIdx = 0
         self.timeout_thread = None
         self.init_timeout()
-        self.capacity = 10
-        self.cache = LRUCache(capacity=self.capacity)
+        self.cache = Cache()
         self.vote_requests_sent = set()
         self.log_dir = f'./logs_node_{self.addr[-1]}'
         self.commitTill = [0]*5
@@ -173,7 +115,7 @@ class Node():
             log_dir = f'./logs_node_{self.addr[-1]}'
             write_to_log(f"SET {key} {value} {self.term}\n", log_dir)
             write_to_metadata(f'log[] - {self.term} SET {key} {value}\n', log_dir)
-        self.cache.getallkeys()
+        self.cache.printcache()
 
     # increment only when we are candidate and receive positve vote
     # change status to LEADER and start heartbeat as soon as we reach majority
@@ -545,7 +487,7 @@ class Node():
             cache_update = True
         if cache_update:
             self.cache.set(key, value)
-            self.cache.getallkeys()
+            self.cache.printcache()
         # put newly inserted key-value pair into local cache
 
         # empty the staged so we can vote accordingly if there is a tie
